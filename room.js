18b8c83131c6687638e5c8f13724d347274b18e2
Nazia <94264778+Nazia-P@users.noreply.github.com>
2023-07-12 11:56:59 +0530

    Update room.js

--- room.js
+++ room.js
@@ -7,5 +7,29 @@ function setGame() {
         let spider = document.createElement("div");
         spider.id = i.toString();
         document.getElementById("board").appendChild(spider);
+        var element = document.getElementById(i.toString());
+        // element.addEventListener('onmouseenter', clearspider);
+        var rect = element.getBoundingClientRect();
+        var elementX = rect.left;
+        var elementY = rect.top;
+        var elementWidth = rect.width;
+        var elementHeight = rect.height;
+        var x, y;
+        webgazer.setGazeListener(function (data, elapsedTime) {
+            x = data.x;
+            y = data.y;
+        })
+        var isGazeEnteringElement = x >= elementX && x <= (elementX + elementWidth) &&
+            y >= elementY && y <= (elementY + elementHeight);
+        if (isGazeEnteringElement) {
+            customevent = new CustomEvent('gazeenter', {bubbles: true,
+            cancelable: true});
+            document.dispatchEvent(gazeenterEvent);
+        }
+        element.addEventListener('gazeenter', clearspider);
     }
+}
+
+function clearspider() {
+    console.log("click");
 }
